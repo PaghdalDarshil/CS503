36c88f96d0181d36b57f9a7dc7dd804e6f2da6d0
PaghdalDarshil <158665973+PaghdalDarshil@users.noreply.github.com>
2025-03-03 17:42:02 -0500

    Update dshlib.h

--- week 7/dshlib.h
+++ week 7/dshlib.h
@@ -1,10 +1,12 @@
 #ifndef __DSHLIB_H__
-#define __DSHLIB_H__
+    #define __DSHLIB_H__
 
-// Constants for command structure sizes
+
+//Constants for command structure sizes
 #define EXE_MAX 64
 #define ARG_MAX 256
 #define CMD_MAX 8
+#define CMD_ARGV_MAX (CMD_MAX + 1)
 // Longest command that can be read from the shell
 #define SH_CMD_MAX EXE_MAX + ARG_MAX
 
@@ -14,37 +16,78 @@ typedef struct command
     char args[ARG_MAX];
 } command_t;
 
-typedef struct command_list
+typedef struct cmd_buff
 {
-    int num;
-    command_t commands[CMD_MAX];
-} command_list_t;
+    int  argc;
+    char *argv[CMD_ARGV_MAX];
+    char *_cmd_buffer;
+} cmd_buff_t;
 
-// Special character #defines
-#define SPACE_CHAR ' '
-#define PIPE_CHAR '|'
+/* WIP - Move to next assignment 
+#define N_ARG_MAX    15     //MAX number of args for a command
+typedef struct command{
+    char exe [EXE_MAX];
+    char args[ARG_MAX];
+    int  argc;
+    char *argv[N_ARG_MAX + 1];  //last argv[LAST] must be \0
+}command_t;
+*/
+
+typedef struct command_list{
+    int num;
+    cmd_buff_t commands[CMD_MAX];
+}command_list_t;
+
+//Special character #defines
+#define SPACE_CHAR  ' '
+#define PIPE_CHAR   '|'
 #define PIPE_STRING "|"
 
-#define SH_PROMPT "dsh> "
+#define SH_PROMPT "dsh3> "
 #define EXIT_CMD "exit"
+#define EXIT_SC     99
 
-// Standard Return Codes
-#define OK 0
-#define WARN_NO_CMDS -1
-#define ERR_TOO_MANY_COMMANDS -2
+//Standard Return Codes
+#define OK                       0
+#define WARN_NO_CMDS            -1
+#define ERR_TOO_MANY_COMMANDS   -2
 #define ERR_CMD_OR_ARGS_TOO_BIG -3
+#define ERR_CMD_ARGS_BAD        -4      //for extra credit
+#define ERR_MEMORY              -5
+#define ERR_EXEC_CMD            -6
+#define OK_EXIT                 -7
 
-// starter code
-#define M_NOT_IMPL "The requested operation is not implemented yet!\n"
-#define EXIT_NOT_IMPL 3
-#define NOT_IMPLEMENTED_YET 0
-
-// prototypes
+//prototypes
+int alloc_cmd_buff(cmd_buff_t *cmd_buff);
+int free_cmd_buff(cmd_buff_t *cmd_buff);
+int clear_cmd_buff(cmd_buff_t *cmd_buff);
+int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff);
+int close_cmd_buff(cmd_buff_t *cmd_buff);
 int build_cmd_list(char *cmd_line, command_list_t *clist);
+int free_cmd_list(command_list_t *cmd_lst);
 
-// output constants
-#define CMD_OK_HEADER "PARSED COMMAND LINE - TOTAL COMMANDS %d\n"
-#define CMD_WARN_NO_CMD "warning: no commands provided\n"
-#define CMD_ERR_PIPE_LIMIT "error: piping limited to %d commands\n"
+//built in command stuff
+typedef enum {
+    BI_CMD_EXIT,
+    BI_CMD_DRAGON,
+    BI_CMD_CD,
+    BI_NOT_BI,
+    BI_EXECUTED,
+} Built_In_Cmds;
+Built_In_Cmds match_command(const char *input); 
+Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd);
+
+//main execution context
+int exec_local_cmd_loop();
+int exec_cmd(cmd_buff_t *cmd);
+int execute_pipeline(command_list_t *clist);
+
+
+
+
+//output constants
+#define CMD_OK_HEADER       "PARSED COMMAND LINE - TOTAL COMMANDS %d\n"
+#define CMD_WARN_NO_CMD     "warning: no commands provided\n"
+#define CMD_ERR_PIPE_LIMIT  "error: piping limited to %d commands\n"
 
 #endif
