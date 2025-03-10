af510a8b17e1ce8944c17403f0bdb77043162ff5
PaghdalDarshil <158665973+PaghdalDarshil@users.noreply.github.com>
2025-03-09 23:00:02 -0400

    completed

--- week 7/dshlib.c
+++ week 7/dshlib.c
@@ -56,220 +56,199 @@
 int last_return_code = 0;
 
 int alloc_cmd_buff(cmd_buff_t *cmd_buff) {
-    cmd_buff->_cmd_buffer = malloc(SH_CMD_MAX + 1);
-    if (!cmd_buff->_cmd_buffer) 
-        return ERR_MEMORY;
-    cmd_buff->argc = 0;
-    memset(cmd_buff->argv, 0, sizeof(cmd_buff->argv));
+    cmd_buff->_cmd_buffer = (char *)malloc(SH_CMD_MAX);
+    if (!cmd_buff->_cmd_buffer) return ERR_MEMORY;
+    memset(cmd_buff, 0, sizeof(cmd_buff_t));
     return OK;
 }
 
 int free_cmd_buff(cmd_buff_t *cmd_buff) {
     if (cmd_buff->_cmd_buffer) {
         free(cmd_buff->_cmd_buffer);
+        cmd_buff->_cmd_buffer = NULL;
     }
-    cmd_buff->_cmd_buffer = NULL;
-    cmd_buff->argc = 0;
+    return OK;
+}
+
+int clear_cmd_buff(cmd_buff_t *cmd_buff) {
+    memset(cmd_buff, 0, sizeof(cmd_buff_t));
     return OK;
 }
 
 int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff) {
-    if (!cmd_line || !cmd_buff) return ERR_CMD_ARGS_BAD;
+    cmd_buff->argc = 0;
+    char *ptr = cmd_line;
+    int in_quotes = 0;
+    char *token_start = NULL;
 
-    if (strlen(cmd_line) > SH_CMD_MAX) {
-        fprintf(stderr, "Error: Command too long.\n");
-        last_return_code = 127;  
-        return ERR_CMD_ARGS_BAD;
-    }        
-
-    char *input = cmd_line;
-    int num_tokens = 0;
-    int in_quote = 0;
-    
-    while (isspace(*input)) input++;
-    
-    if (*input == '\0') 
-        return WARN_NO_CMDS;
-
-    char *current = input;
-    char *token_start = input;
-    char *tokens[CMD_ARGV_MAX] = { NULL };
-
-    while (*current) {
-        if (*current == '"') {
-            in_quote = !in_quote;
-            if (!in_quote) {
-                tokens[num_tokens] = strndup(token_start, current - token_start);
-                if (++num_tokens >= CMD_ARGV_MAX) goto too_many_tokens;
-                while (isspace(*++current)) current++;
-                token_start = current;
-                continue;
-            }
-            token_start = current + 1;
+    while (*ptr) {
+        while (isspace(*ptr) && !in_quotes) ptr++;
+        if (*ptr == '"') {
+            in_quotes = !in_quotes;
+            ptr++;
+            token_start = ptr;
+        } else {
+            token_start = ptr;
         }
-        else if (!in_quote && isspace(*current)) {
-            if (current > token_start) {
-                tokens[num_tokens] = strndup(token_start, current - token_start);
-                if (++num_tokens >= CMD_ARGV_MAX) goto too_many_tokens;
+
+        while (*ptr && (in_quotes || !isspace(*ptr))) {
+            if (*ptr == '"') {
+                in_quotes = !in_quotes;
+                *ptr = '\0';
             }
-            while (isspace(*current)) current++;
-            token_start = current;
-            continue;
+            ptr++;
         }
-        current++;
-    }
 
-    if (current > token_start && !in_quote) {
-        tokens[num_tokens] = strndup(token_start, current - token_start);
-        if (++num_tokens >= CMD_ARGV_MAX) goto too_many_tokens;
-    }
+        if (*ptr) {
+            *ptr = '\0';
+            ptr++;
+        }
 
-    if (in_quote) {
-        for (int i = 0; i < num_tokens; i++) free(tokens[i]);
-        return ERR_CMD_ARGS_BAD;
+        cmd_buff->argv[cmd_buff->argc++] = token_start;
+        if (cmd_buff->argc >= CMD_ARGV_MAX - 1) {
+            fprintf(stderr, "error: too many arguments\n");
+            return ERR_CMD_ARGS_BAD;
+        }
     }
-
-    free_cmd_buff(cmd_buff);
-    cmd_buff->_cmd_buffer = malloc(SH_CMD_MAX);
-    if (!cmd_buff->_cmd_buffer) {
-        for (int i = 0; i < num_tokens; i++) free(tokens[i]);
-        return ERR_MEMORY;
-    }
-
-    char *ptr = cmd_buff->_cmd_buffer;
-    cmd_buff->argc = num_tokens;
-    for (int i = 0; i < num_tokens; i++) {
-        strcpy(ptr, tokens[i]);
-        cmd_buff->argv[i] = ptr;
-        ptr += strlen(tokens[i]) + 1;
-        free(tokens[i]);
-    }
-    cmd_buff->argv[num_tokens] = NULL;
+    cmd_buff->argv[cmd_buff->argc] = NULL;
     return OK;
-
-too_many_tokens:
-    for (int i = 0; i < num_tokens; i++) free(tokens[i]);
-    return ERR_TOO_MANY_COMMANDS;
 }
 
 int build_cmd_list(char *cmd_line, command_list_t *clist) {
-    if (!cmd_line || !clist) return ERR_CMD_ARGS_BAD;
-
     clist->num = 0;
-    char *token = strtok(cmd_line, PIPE_STRING);
+    char *saveptr;
+    char *token = strtok_r(cmd_line, "|", &saveptr);
 
     while (token != NULL) {
         if (clist->num >= CMD_MAX) {
+            fprintf(stderr, CMD_ERR_PIPE_LIMIT, CMD_MAX);
             return ERR_TOO_MANY_COMMANDS;
         }
 
-        while (isspace(*token)) token++;
-        char *end = token + strlen(token) - 1;
-        while (end > token && isspace(*end)) *end-- = '\0';
+        while (*token == ' ') token++;
+        size_t len = strlen(token);
+        while (len > 0 && token[len - 1] == ' ') token[--len] = '\0';
 
-        build_cmd_buff(token, &clist->commands[clist->num]);
+        cmd_buff_t *cmd = &clist->commands[clist->num];
+        if (alloc_cmd_buff(cmd) != OK) return ERR_MEMORY;
+
+        int rc = build_cmd_buff(token, cmd);
+        if (rc != OK) {
+            free_cmd_buff(cmd);
+            return rc;
+        }
+
         clist->num++;
-
-        token = strtok(NULL, PIPE_STRING);
+        token = strtok_r(NULL, "|", &saveptr);
     }
 
+    if (clist->num == 0) {
+        printf(CMD_WARN_NO_CMD);
+        return WARN_NO_CMDS;
+    }
     return OK;
 }
 
+int free_cmd_list(command_list_t *clist) {
+    for (int i = 0; i < clist->num; i++) {
+        free_cmd_buff(&clist->commands[i]);
+    }
+    clist->num = 0;
+    return OK;
+}
+
+Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd) {
+    if (strcmp(cmd->argv[0], "exit") == 0) {
+        free_cmd_buff(cmd);
+        exit(0);
+    }
+
+    if (strcmp(cmd->argv[0], "cd") == 0) {
+        if (cmd->argc > 2) {
+            fprintf(stderr, "error: cd command accepts at most 1 argument\n");
+            return ERR_CMD_ARGS_BAD;
+        }
+        if (chdir(cmd->argv[1]) != 0) {
+            perror("cd failed");
+        }
+        return BI_EXECUTED;
+    }
+
+    return BI_NOT_BI;
+}
+
 int execute_pipeline(command_list_t *clist) {
-    int pipes[CMD_MAX - 1][2];
-    pid_t pids[CMD_MAX];
-    int exit_status = 0; 
+    int num_cmds = clist->num;
+    int prev_pipe = -1;
+    int fd[2];
+    pid_t pids[num_cmds];
 
-    for (int i = 0; i < clist->num - 1; i++) {
-        if (pipe(pipes[i]) < 0) {
-            perror("pipe error");
+    for (int i = 0; i < num_cmds; i++) {
+        if (i < num_cmds - 1 && pipe(fd) < 0) {
+            perror("pipe");
             return ERR_EXEC_CMD;
         }
-    }
 
-    for (int i = 0; i < clist->num; i++) {
         pids[i] = fork();
-
         if (pids[i] < 0) {
-            perror("fork error");
+            perror("fork");
             return ERR_EXEC_CMD;
         }
 
-        if (pids[i] == 0) {  
-            if (i > 0) dup2(pipes[i - 1][0], STDIN_FILENO);
-            if (i < clist->num - 1) dup2(pipes[i][1], STDOUT_FILENO);
-
-            for (int j = 0; j < clist->num - 1; j++) {
-                close(pipes[j][0]);
-                close(pipes[j][1]);
+        if (pids[i] == 0) { // Child
+            if (i > 0) {
+                dup2(prev_pipe, STDIN_FILENO);
+                close(prev_pipe);
             }
-
+            if (i < num_cmds - 1) {
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+            }
             execvp(clist->commands[i].argv[0], clist->commands[i].argv);
-            perror("exec failed");
-            _exit(127);  
+            perror("execvp failed");
+            exit(ERR_EXEC_CMD);
+        } else { // Parent
+            if (i > 0) close(prev_pipe);
+            if (i < num_cmds - 1) {
+                prev_pipe = fd[0];
+                close(fd[1]);
+            }
         }
     }
 
-    for (int i = 0; i < clist->num - 1; i++) {
-        close(pipes[i][0]);
-        close(pipes[i][1]);
+    for (int i = 0; i < num_cmds; i++) {
+        waitpid(pids[i], NULL, 0);
     }
-
-    for (int i = 0; i < clist->num; i++) {
-        int status;
-        waitpid(pids[i], &status, 0);
-        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-            exit_status = WEXITSTATUS(status);
-        }
-    }
-
-    return exit_status;
+    return OK;
 }
 
 int exec_local_cmd_loop() {
-    char input[SH_CMD_MAX];
-    command_list_t cmd_list;
+    char input_buffer[SH_CMD_MAX];
+    command_list_t clist;
 
     while (1) {
         printf("%s", SH_PROMPT);
-        if (!fgets(input, SH_CMD_MAX, stdin)) {
+        if (fgets(input_buffer, SH_CMD_MAX, stdin) == NULL) {
             printf("\n");
             break;
         }
-        input[strcspn(input, "\n")] = '\0';
+        input_buffer[strcspn(input_buffer, "\n")] = '\0';
+        if (strlen(input_buffer) == 0) continue;
 
-        if (strlen(input) == 0) {
-            fprintf(stderr, "%s\n", CMD_WARN_NO_CMD);
-            last_return_code = 0; 
-            continue;
-        }                
-
-        if (strcmp(input, EXIT_CMD) == 0) {
-            printf("exiting...\n");
-            break;
-        }
-
-        if (strncmp(input, "cd ", 3) == 0) {
-            char *dir = input + 3;
-            while (isspace(*dir)) dir++;
-            if (chdir(dir) != 0) {
-                perror("cd error");
-            }
+        int rc = build_cmd_list(input_buffer, &clist);
+        if (rc != OK) {
+            if (rc == WARN_NO_CMDS) printf(CMD_WARN_NO_CMD);
+            else if (rc == ERR_TOO_MANY_COMMANDS) printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
             continue;
         }
 
-        int parse_status = build_cmd_list(input, &cmd_list);
-        if (parse_status == WARN_NO_CMDS) {
-            fprintf(stderr, "%s", CMD_WARN_NO_CMD);
-            continue;
-        } else if (parse_status == ERR_TOO_MANY_COMMANDS) {
-            fprintf(stderr, "%s", CMD_ERR_PIPE_LIMIT);
+        if (clist.num == 1 && exec_built_in_cmd(&clist.commands[0]) == BI_EXECUTED) {
+            free_cmd_list(&clist);
             continue;
         }
 
-        execute_pipeline(&cmd_list);
+        execute_pipeline(&clist);
+        free_cmd_list(&clist);
     }
-
     return OK;
 }
